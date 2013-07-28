9025c2bdd3fcb5eff3ac8a70b9892c0568a0807b
Stefan Krist <stefan@bulletproof.net>
2013-07-28 23:13:49 +1000

    FIXME comment RE using screen

--- ripper.sh
+++ ripper.sh
@@ -190,6 +190,7 @@ ripTitle() {
 	# Change into TMPDIR due to divx2pass.log
 	cd "$TMPDIR"
 	pwd
+	#FIXME: This should probably run in a screen sessions? Maybe write a screen config to run these commands?
 	echo mencoder -dvd-device "$MYDIR/$DVD_NAME" dvd://"$DVD_TITLE" -profile "$PROFILE" -xvidencopts pass=1 -o /dev/null
 	mencoder -dvd-device "$MYDIR/$DVD_NAME" dvd://"$DVD_TITLE" -profile "$PROFILE" -xvidencopts pass=1 -o /dev/null
 	mencoder -dvd-device "$MYDIR/$DVD_NAME" dvd://"$DVD_TITLE" -profile "$PROFILE" -xvidencopts pass=2 -o "`echo $DVD_NAME.$DVD_TITLE| sed -e 's/\.iso//'`".avi 
